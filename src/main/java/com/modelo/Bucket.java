314d0533061c0b48ceae7cbdd17d9b4fddb52822
rbreuer <rbreuer@rbreuer-HP-Pavilion-dv4-Notebook-PC>
2018-01-30 17:06:07 -0500

    1- minio administrator

---
+++ src/main/java/com/modelo/Bucket.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modelo;
+
+/**
+ *
+ * @author rbreuer
+ */
+public class Bucket {
+    
+    void mosrarListaBuckets(){}
+    void resetModel(){}
+    void crearBucket(){}
+    void eliminarBucketLista(){}
+    void eliminarBucketMenu(){}
+    void mostrarArchivosBucket(){}
+    void verLogs(){}
+    
+    
+}
---
+++ src/main/java/com/modelo/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modelo;
+
+import com.vista.MainFrame;
+import io.minio.errors.ErrorResponseException;
+import io.minio.errors.InsufficientDataException;
+import io.minio.errors.InternalException;
+import io.minio.errors.InvalidBucketNameException;
+import io.minio.errors.NoResponseException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import org.xmlpull.v1.XmlPullParserException;
+
+/**
+ *
+ * @author rbreuer
+ */
+public class Main {
+    public static void main (String[]args) throws InvalidBucketNameException, NoSuchAlgorithmException, InsufficientDataException, IOException, InvalidKeyException, NoResponseException, XmlPullParserException, ErrorResponseException, InternalException{
+    MainFrame mf;
+        mf = new MainFrame();
+    }
+}
